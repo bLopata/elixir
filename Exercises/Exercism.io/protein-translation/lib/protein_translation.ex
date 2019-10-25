4ff4d69b9e3e249f5ce4d82a345055cab3b906ad
Ben Lopata <benlopata@gmail.com>
2019-10-25 12:10:55 -0500

    Completed solution for protein translation exercise using try catch logic and Enum.reduce_while/3

--- Exercises/Exercism.io/protein-translation/lib/protein_translation.ex
+++ Exercises/Exercism.io/protein-translation/lib/protein_translation.ex
@@ -24,34 +24,29 @@ defmodule ProteinTranslation do
   """
   @spec of_rna(String.t()) :: {atom, list(String.t())}
   def of_rna(rna) do
+    case transcribe(rna) do
+      :invalid -> {:error, "invalid RNA"}
+      proteins -> {:ok, proteins |> Enum.reverse}
     # Parses RNA string into array of 3-bytechar substrings
-    parsed_rna = for <<x::binary-3 <- rna>>, do: x
-    for rna <- parsed_rna, do: of_codon(rna)
+    end
   end
 
-  @doc """
-  Given a codon, return the corresponding protein
+  defp transcribe(rna) do
+    Regex.scan(~r/.{3}/, rna) |> Enum.reduce_while([], fn [codon], proteins ->
+      case of_codon(codon) do
+        { :ok, "STOP" }  -> { :halt, proteins }
+        { :ok, protein } -> { :cont, [ protein | proteins ] }
+        { :error, _ }    -> { :halt, :invalid }
+      end
+    end)
+  end
 
-  UGU -> Cysteine
-  UGC -> Cysteine
-  UUA -> Leucine
-  UUG -> Leucine
-  AUG -> Methionine
-  UUU -> Phenylalanine
-  UUC -> Phenylalanine
-  UCU -> Serine
-  UCC -> Serine
-  UCA -> Serine
-  UCG -> Serine
-  UGG -> Tryptophan
-  UAU -> Tyrosine
-  UAC -> Tyrosine
-  UAA -> STOP
-  UAG -> STOP
-  UGA -> STOP
-  """
   @spec of_codon(String.t()) :: {atom, String.t()}
   def of_codon(codon) do
-    Map.fetch(@mapping, codon)
+    try do
+      {:ok, Map.fetch!(@mapping, codon)}
+    rescue
+      KeyError -> {:error, "invalid codon"}
+    end
   end
 end

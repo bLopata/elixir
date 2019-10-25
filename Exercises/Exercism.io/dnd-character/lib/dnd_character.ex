b20ba3421f5aa32b1f832c74866cc6fee5a9aef4
Ben Lopata <benlopata@gmail.com>
2019-10-25 16:58:47 -0500

    Added completed solution using structs and Integer.floor_div/2.

--- Exercises/Exercism.io/dnd-character/lib/dnd_character.ex
+++ Exercises/Exercism.io/dnd-character/lib/dnd_character.ex
@@ -13,13 +13,28 @@ defmodule DndCharacter do
 
   @spec modifier(pos_integer()) :: integer()
   def modifier(score) do
+    Integer.floor_div(score - 10, 2)
   end
 
   @spec ability :: pos_integer()
   def ability do
+    for(_ <- 1..4, do: Enum.random(1..6))
+    |> Enum.sort
+    |> Enum.drop(1)
+    |> Enum.sum
   end
 
   @spec character :: t()
   def character do
+    constitution = ability()
+    %{
+      strength: ability(),
+      dexterity: ability(),
+      constitution: constitution,
+      intelligence: ability(),
+      wisdom: ability(),
+      charisma: ability(),
+      hitpoints: 10 + modifier(constitution)
+    }
   end
 end

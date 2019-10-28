4e5861f2353c78b7158c71a58173d31b9aca8f14
Ben Lopata <benlopata@gmail.com>
2019-10-28 11:30:02 -0500

    Completed solution to twelve days exercise using comprehensions and a single heavy-lifter function.

--- Exercises/Exercism.io/twelve-days/lib/twelve_days.ex
+++ Exercises/Exercism.io/twelve-days/lib/twelve_days.ex
@@ -1,15 +1,15 @@
 defmodule TwelveDays do
   @gifts [
-    {1, "first", "a partridge in a pear tree"},
+    {1, "first", "a Partridge in a Pear Tree"},
     {2, "second", "two Turtle Doves"},
     {3, "third", "three French Hens"},
-    {4, "fourth", " four Calling Birds"},
-    {5, "fifth", "five Gold rings"},
+    {4, "fourth", "four Calling Birds"},
+    {5, "fifth", "five Gold Rings"},
     {6, "sixth", "six Geese-a-Laying"},
     {7, "seventh", "seven Swans-a-Swimming"},
     {8, "eighth", "eight Maids-a-Milking"},
     {9, "ninth", "nine Ladies Dancing"},
-    {10, "tenth", "ten Lords-a-Leaping, "},
+    {10, "tenth", "ten Lords-a-Leaping"},
     {11, "eleventh", "eleven Pipers Piping"},
     {12, "twelfth", "twelve Drummers Drumming"}
   ]
@@ -19,10 +19,17 @@ defmodule TwelveDays do
   all gifts for previous days in the same line.
   """
   @spec verse(number :: integer) :: String.t()
-  def verse(number) do
-    for {num, day, presents} when num < number <- @gifts do
-      for present <- presents, do: Enum.intersperse(present, ", ")
+  def verse(start_num \\ 1, number) do
+    list = for {num, _day, presents} when start_num <= num and num <= number <- @gifts, into: [], do: presents
+    {_, day, _} = List.keyfind(@gifts, number, 0)
+    cond do
+      length(list) > 1 ->
+        list = list |> Enum.map_every(number, fn x -> "and " <> x end) |> Enum.reverse() |> Enum.intersperse(", ")
+        "On the #{day} day of Christmas my true love gave to me: #{list}."
+      length(list) == 1 ->
+        "On the #{day} day of Christmas my true love gave to me: #{list}."
     end
+
   end
 
   @doc """
@@ -31,6 +38,8 @@ defmodule TwelveDays do
   """
   @spec verses(starting_verse :: integer, ending_verse :: integer) :: String.t()
   def verses(starting_verse, ending_verse) do
+    out = for num <- starting_verse..ending_verse, do: verse(num)
+    Enum.join(out, "\n")
   end
 
   @doc """
@@ -38,5 +47,7 @@ defmodule TwelveDays do
   """
   @spec sing() :: String.t()
   def sing do
+    out = for num <- 1..12, do: verse(num)
+    Enum.join(out, "\n")
   end
 end

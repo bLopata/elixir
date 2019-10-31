560e2bc27e971edfd2c9b78ed5836fb92b8ed344
Ben Lopata <benlopata@gmail.com>
2019-10-31 10:56:44 -0500

    Completed solution using Regex.scan/2 to create the list of characters and Enum.uniq/1 to check if the string is an isogram.

--- Exercises/Exercism.io/isogram/lib/isogram.ex
+++ Exercises/Exercism.io/isogram/lib/isogram.ex
@@ -4,10 +4,7 @@ defmodule Isogram do
   """
   @spec isogram?(String.t()) :: boolean
   def isogram?(sentence) do
-    char_count = String.downcase(sentence)
-    |> String.replace(~r/[\s-]/, "")
-    |> String.split("")
-    |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, & &1+1) end)
-    if((for c <- Map.keys(char_count), do: Map.fetch!(char_count, c)) > 0, do: false, else: true)
+    words = Regex.scan(~r/[[:alpha:]]/u, sentence)
+    Enum.uniq(words) == words
   end
 end

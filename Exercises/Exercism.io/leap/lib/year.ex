1680b4d74dc52b111fcf283c42821b27be7d50e6
Ben Lopata <benlopata@gmail.com>
2019-11-12 16:36:57 -0600

    Updated formatting.

--- Exercises/Exercism.io/leap/lib/year.ex
+++ Exercises/Exercism.io/leap/lib/year.ex
@@ -14,7 +14,7 @@ defmodule Year do
       0, 0, 0 -> true
       0, 0, _ -> false
       0, _, _ -> true
-      _,_,_ -> false
+      _, _, _ -> false
     end
 
     check.(rem(year, 4), rem(year, 100), rem(year, 400))
